a7b2e0bb0156b9ffd0329e2be5ca1d87f03b6343
Maricifu <152333294+Maricifu@users.noreply.github.com>
2024-02-07 21:37:06 -0500

    avances

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,10 +4,3 @@ let gato = document.querySelector("#usuario");
 user.addEventListener("click",function(){
     gato.classList.toggle("active");
 });
-
-let homem = document.querySelector(".1");
-let homep = document.querySelector("#home");
-
-homem.addEventListener("click",function(){
-    homep.classList.toggle("active");
-});
